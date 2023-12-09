1d2c29e4aeef49220f2fb58166a16469a0c8c8bd
dipada <daniele.dipalma68@gmail.com>
2023-12-09 19:59:08 +0100

    added User model class

---
+++ userservice/src/main/java/com/userservice/userservice/model/User.java
@@ -0,0 +1,73 @@
+package com.userservice.userservice.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "nome")
+    private String name;
+
+    @Column(unique = true, name = "email")
+    private String email;
+
+   // private UserType userType;
+    public User() {
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email=email;
+    }
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
