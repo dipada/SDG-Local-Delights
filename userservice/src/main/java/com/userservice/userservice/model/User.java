0a56f86de7837473168a3349f77f761da96aeb0f
dipada <daniele.dipalma68@gmail.com>
2023-12-10 10:41:27 +0100

    refactor lombok

--- userservice/src/main/java/com/userservice/userservice/model/User.java
+++ userservice/src/main/java/com/userservice/userservice/model/User.java
@@ -1,9 +1,13 @@
 package com.userservice.userservice.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
+
+
 
 @Entity
 @Table(name = "_user")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -14,59 +18,5 @@ public class User {
     @Column(unique = true, name = "email")
     private String email;
 
-   // private UserType userType;
-    public User() {
-    }
-
-    public User(int id) {
-        this.id = id;
-    }
-
-    public User(String name) {
-        this.name = name;
-    }
-
-    public User(String name, String email) {
-        this.name = name;
-        this.email=email;
-    }
-
-    public User(int id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+   //TODO private UserType userType;
 }
