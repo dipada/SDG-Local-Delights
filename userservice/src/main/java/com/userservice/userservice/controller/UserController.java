292176eeba332602f4e0aa49c97986800751af53
dipada <daniele.dipalma68@gmail.com>
2023-12-10 13:00:27 +0100

    added openAPI support, some operation declaration on controller methods, integration with local swagger ui and openAPI dog endpoint

--- userservice/src/main/java/com/userservice/userservice/controller/UserController.java
+++ userservice/src/main/java/com/userservice/userservice/controller/UserController.java
@@ -2,7 +2,12 @@ package com.userservice.userservice.controller;
 
 import com.userservice.userservice.model.User;
 import com.userservice.userservice.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,26 +25,41 @@ public class UserController {
     }
 
 
+    @Operation(summary = "Get user by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the user"),
+            @ApiResponse(responseCode = "400", description = "Invalid email supplied"),
+            @ApiResponse(responseCode = "404", description = "User not found")})
     @GetMapping()
-    public ResponseEntity<Optional<User>> getUser(@RequestParam String email) {
+    public ResponseEntity<Optional<User>> getUserByEmail(@RequestParam String email) {
         Optional<User> user = userRepository.findUserByEmail(email);
         if (user.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(user);
-
     }
 
+    @Operation(summary = "Get all users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all users")})
     @GetMapping("/all")
     public ResponseEntity<Iterable<User>> getAllUsers() {
         Iterable<User> users = userRepository.findAll();
         return ResponseEntity.ok(users);
     }
 
+    @Operation(summary = "Create new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User created successfully"),
+            @ApiResponse(responseCode = "409", description = "Email already exists")})
     @PostMapping()
-    public ResponseEntity<User> addUser(@RequestBody User user) {
+    public ResponseEntity<String> addUser(@RequestBody @NotNull User user) {
         //TODO check if email already exists and return 409
+        Optional<User> userByEmail = userRepository.findUserByEmail(user.getEmail());
+        if (userByEmail.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("email already exists");
+        }
         userRepository.save(user);
-        return ResponseEntity.ok(user);
+        return ResponseEntity.ok("user added successfully");
     }
 }
