d2a4abb4685e75196612fc10d9848fb4200fa885
dipada <daniele.dipalma68@gmail.com>
2023-12-10 20:01:24 +0100

    added oauth2 authentication scaffold. To end or convert to JWT token.

---
+++ authenticationservice/src/test/java/com/authentication/authenticationservice/AuthenticationserviceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.authentication.authenticationservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationserviceApplicationTests {
+
+    //@Test
+    void contextLoads() {
+    }
+
+}
