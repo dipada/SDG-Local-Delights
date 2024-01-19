01e06eb980dc91754d9f4c80b9adafb103310535
dipada <daniele.dipalma68@gmail.com>
2024-01-19 17:57:08 +0100

    init auth

--- authenticationservice/src/main/java/com/authentication/authenticationservice/config/SecurityConfig.java
+++ authenticationservice/src/main/java/com/authentication/authenticationservice/config/SecurityConfig.java
@@ -7,11 +7,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+/*
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         return http
                 .authorizeHttpRequests( auth -> {auth.requestMatchers("/").permitAll();
                         auth.anyRequest().authenticated();
@@ -19,5 +21,46 @@ public class SecurityConfig {
                 .oauth2Login(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .build();
+
+
+
+    }
+    */
+
+
+    /*
+    .formLogin(form -> form
+                        .loginPage("http://localhost:8081/api/v1/user/seller/daniele@gmail.com")
+                        .permitAll()
+     */
+
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            .requestMatchers("/").permitAll();
+                    authorize.anyRequest().authenticated();
+                })
+                .oauth2Login(oauth2 -> oauth2
+                        //.loginPage("/login")
+                        //.loginPage("http://localhost:8081/api/v1/user/seller/daniele@gmail.com")
+                        .defaultSuccessUrl("https://www.youtube.com/watch?v=KSXdqmCusmY&ab_channel=YellowFellow", true)
+                        //.successHandler((request, response, authentication) -> {
+                        //  response.sendRedirect("https://www.youtube.com/watch?v=KSXdqmCusmY&ab_channel=YellowFellow");
+                        //})
+                )
+        .build();
     }
 }
+
+
+
+
