6345a6751d159a383145805067d9c62ca30d7773
dipada <daniele.dipalma68@gmail.com>
2024-01-21 13:25:47 +0100

    added default vue-tailwind component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import HeroSection from "@/components/HeroSection.vue";
 </script>
 
 <template>
@@ -18,6 +19,7 @@ import HelloWorld from './components/HelloWorld.vue'
   </header>
 
   <RouterView />
+  <HeroSection />
 </template>
 
 <style scoped>
