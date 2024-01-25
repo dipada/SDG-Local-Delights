6ebc26b92558980581cdec7f769bb6ec29ad1b37
dipada <daniele.dipalma68@gmail.com>
2024-01-25 17:52:56 +0100

    client home view

--- frontend/src/components/ClientHomeBodyComponent.vue
+++ frontend/src/components/ClientHomeBodyComponent.vue
@@ -1,40 +1,71 @@
 <template>
-  <div class="flex flex-col items-center max-w-[709px] rounded-2xl border-t border-l-[1.5px] border-r-[0.5px] border-b-2 border-solid border-neutral-700 pb-5">
-    <section class="flex flex-col md:flex-row justify-between items-stretch self-stretch gap-2 p-6 md:px-5">
-      <div class="flex justify-between items-stretch bg-neutral-500 bg-opacity-10 px-6 py-2.5 rounded-lg md:px-5">
-        <h2 class="text-lg font-black leading-5 text-neutral-700">motorcycle</h2>
-        <h2 class="text-lg font-bold leading-5 text-neutral-700">Delivery</h2>
-      </div>
-      <div class="flex justify-between items-stretch px-6 py-2.5 md:px-5">
-        <h2 class="text-lg font-black leading-5 text-neutral-700">shopping-bag</h2>
-        <h2 class="text-lg font-bold leading-5 text-neutral-700">Pickup</h2>
-      </div>
-    </section>
-    <!-- TODO mettere l'immagine delivery e pickup-->
-    <img
-        src=""
-        alt=""
-        class="w-full object-contain object-center rounded-lg overflow-hidden border-neutral-700 border-4"
-        loading="lazy"
-    />
-    <section class="flex flex-col md:flex-row items-center gap-4 mt-6 md:px-5">
-      <div class="flex items-center justify-between bg-neutral-700 bg-opacity-10 px-4 py-2 rounded-lg md:pr-5">
-        <div class="text-2xl font-black leading-8 text-neutral-500 whitespace-nowrap">METTERE ICONA MAPPA</div>
-        <div class="grow self-center text-lg leading-6 text-neutral-700 my-auto whitespace-nowrap">Enter Your Address</div>
-        <!-- TODO rendere input box -->
-      </div>
-      <div class="flex items-center justify-between self-stretch px-12 py-5 rounded-lg md:px-5">
-        <button class="text-sm font-black leading-4 text-white my-auto">SEARCH</button>
-      </div>
-    </section>
+  <div class="flex flex-col items-center justify-center">
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-secondary md:text-5xl lg:text-6xl dark:text-secondary">Within a few clicks, find local products near you</h1>
+    <div class="border border-secondary rounded-lg p-4 max-w-md w-full">
+      <form class="flex-col">
+        <div class="mb-4">
+          <div class="flex items-center mb-2">
+            <label for="delivery" class="text-sm font-medium text-gray-900 dark:text-white mr-2 flex items-center">
+              <svg class="mr-2" fill="#000000" height="20" width="20" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 491.351 491.351" xml:space="preserve">
+                <path id="XMLID_188_" d="M381.559,182.007c-0.895,0-1.755,0.115-2.632,0.138l-14.934-87.322c-0.809-4.710-3.613-8.842-7.674-11.355
+                c-4.051-2.511-8.998-3.154-13.582-1.772l-44.975,13.622c-8.792,2.667-13.763,11.963-11.097,20.771
+                c2.667,8.801,11.992,13.789,20.757,11.111l26.901-8.15l6.167,35.949H198.895v-7.076l17.403-4.393
+                c6.916-1.741,11.845-8.190,11.433-15.575c-0.464-8.369-7.639-14.770-16.018-14.290l-64.285,3.653
+                c-1.282,0.080-2.701,0.292-4.001,0.616c-11.415,2.889-18.349,14.487-15.467,25.906c2.882,11.419,14.495,18.339,25.911,15.458l11.707-2.959v10.815l-13.694,23.586
+                c-12.964-5.610-27.142-8.733-42.032-8.733C49.282,182.007,0,233.227,0,296.184C0,359.151,49.282,410.370,109.852,410.370
+                c55.081,0,100.708-42.453,108.494-97.529h17.763c4.732,0,9.239-2.017,12.396-5.547l24.432-27.287
+                c-0.732,5.304-1.239,10.673-1.239,16.177c0,62.968,49.291,114.187,109.861,114.187c60.543,0,109.792-51.219,109.792-114.187
+                C491.351,233.227,442.102,182.007,381.559,182.007z M109.852,377.058c-42.203,0-76.544-36.280-76.544-80.874
+                c0-44.591,34.341-80.864,76.544-80.864c8.783,0,17.084,1.895,24.956,4.789l-39.365,67.709c-2.994,5.148-3.011,11.508-0.035,16.672
+                c2.959,5.165,8.474,8.352,14.444,8.352h74.824C177.390,349.464,146.620,377.058,109.852,377.058z M184.676,279.528h-45.868
+                l24.078-41.406C173.682,249.151,181.476,263.423,184.676,279.528z M228.660,279.528h-10.314
+                c-4.001-28.383-17.988-53.433-38.255-71.018l11.743-20.202h118.464L228.660,279.528z M381.559,377.058
+                c-42.211,0-76.544-36.280-76.544-80.874c0-33.481,19.355-62.272,46.874-74.536l13.239,77.351c1.403,8.118,8.431,13.842,16.397,13.842
+                c0.945,0,1.892-0.072,2.829-0.236c9.085-1.553,15.166-10.167,13.618-19.232l-13.307-77.720c40.714,1.774,73.378,37.070,73.378,80.531
+                C458.043,340.777,423.719,377.058,381.559,377.058z"/>
+              </svg>
+              Delivery
+            </label>
+            <input type="radio" id="delivery" name="deliveryOption" value="delivery" checked>
+            <label for="pickup" class="text-sm font-medium text-gray-900 dark:text-white mr-2 ml-4 flex items-center">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 text-gray-500 dark:text-gray-400 mr-2">
+                <path d="M9 11V6C9 4.34315 10.3431 3 12 3C13.6569 3 15 4.34315 15 6V10.9673M10.4 21H13.6C15.8402 21 16.9603 21 17.816 20.564C18.5686 20.1805 19.1805 19.5686 19.564 18.816C20 17.9603 20 16.8402 20 14.6V12.2C20 11.0799 20 10.5198 19.782 10.092C19.5903 9.71569 19.2843 9.40973 18.908 9.21799C18.4802 9 17.9201 9 16.8 9H7.2C6.0799 9 5.51984 9 5.09202 9.21799C4.71569 9.40973 4.40973 9.71569 4.21799 10.092C4 10.5198 4 11.0799 4 12.2V14.6C4 16.8402 4 17.9603 4.43597 18.816C4.81947 19.5686 5.43139 20.1805 6.18404 20.564C7.03968 21 8.15979 21 10.4 21Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+              Pickup
+            </label>
+            <input type="radio" id="pickup" name="deliveryOption" value="pickup">
+          </div>
+          <hr class="border-t border-gray-300 dark:border-gray-600">
+        </div>
+        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div class="relative">
+          <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+          </div>
+          <input type="search" id="default-search" class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-secondary focus:border-secondary dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your address" required>
+          <button @click="findShops" type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-secondary hover:bg-secondary focus:ring-4 focus:outline-none focus:ring-secondary font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
-
 
 <script>
 export default {
   name: 'ClientHomeBodyComponent',
+
+  methods: {
+    findShops() {
+      this.$router.push({name: 'client-shops'});
+    }
+  }
+
 }
+
+
+
 </script>
 
 <style>
--- frontend/src/views/client/ClientHomeView.vue
+++ frontend/src/views/client/ClientHomeView.vue
@@ -1,23 +1,83 @@
 <template>
   <HeaderBase>
     <template #nav>
-      <cart-button-component />
-      <AvatarComponent :image-url="userInfo.picture" :name="userInfo.name" />
+      <div class="flex flex-row justify-end px-4 py-2">
+        <AvatarComponent class="pr-4" :image-url="userInfo.picture" :name="userInfo.name"/>
+        <cart-button-component class="pr-4"/>
+        <logout-button-component class="p"/>
+      </div>
     </template>
   </HeaderBase>
-  <client-home-body-component />
+  <div class="flex-col justify-center items-center mx-auto mt-20 min-w-full">
+    <client-home-body-component/>
+  </div>
+  <div class="flex-col justify-center mx-auto items-center">
+    <h2 class="py-10 text-3xl font-bold text-center text-green-800">Maybe you can find:</h2>
+
+    <div class="container flex flex-col md:flex-row justify-center mx-auto items-center">
+      <CardHome class="m-4" :image="fruitImg" title="Fruit"/>
+      <CardHome class="m-4" :image="pastaImg" title="Pasta"/>
+      <CardHome class="m-4" :image="breadImg" title="Bread"/>
+      <CardHome class="m-4" :image="eggsImg" title="Eggs"/>
+      <CardHome class="m-4" :image="meatImg" title="Meat"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent, computed } from "vue";
+import {defineComponent, computed} from "vue";
 import HeaderBase from "@/components/HeaderBase.vue";
 import AvatarComponent from "@/components/avatarComponent.vue";
 import CartButtonComponent from "@/components/cartButtonComponent.vue";
 import ClientHomeBodyComponent from "@/components/ClientHomeBodyComponent.vue";
+import LogoutButtonComponent from "@/components/LogoutButtonComponent.vue";
+import CardHome from "@/components/CardHome.vue";
+import locationDotImg from "@/assets/location-dot-solid.svg";
+import bellCrocierieImg from "@/assets/bell-concierge-solid.svg";
+import creditCardImg from "@/assets/credit-card-solid.svg";
+import shoppingBagImg from "@/assets/shoppingBag.svg";
+import fruitImg from "@/assets/fruit.svg";
+import vegetablesImg from "@/assets/vegetables.svg";
+import pastaImg from "@/assets/pasta.svg";
+import breadImg from "@/assets/bread.svg";
+import eggsImg from "@/assets/eggs.svg";
+import meatImg from "@/assets/meat.svg";
 
 export default defineComponent({
-  components: { ClientHomeBodyComponent, CartButtonComponent, HeaderBase, AvatarComponent },
+  components: {
+    CardHome,
+    LogoutButtonComponent, ClientHomeBodyComponent, CartButtonComponent, HeaderBase, AvatarComponent},
   computed: {
+    meatImg() {
+      return meatImg
+    },
+    eggsImg() {
+      return eggsImg
+    },
+    breadImg() {
+      return breadImg
+    },
+    pastaImg() {
+      return pastaImg
+    },
+    vegetablesImg() {
+      return vegetablesImg
+    },
+    fruitImg() {
+      return fruitImg
+    },
+    shoppingBagImg() {
+      return shoppingBagImg
+    },
+    creditCardImg() {
+      return creditCardImg
+    },
+    bellCrocierieImg() {
+      return bellCrocierieImg
+    },
+    locationDotImg() {
+      return locationDotImg
+    },
     userInfo() {
       return this.$store.state.userInfo;
     }
