84ea92ad1961dad34de3eeea19510055bb17bf73
dipada <daniele.dipalma68@gmail.com>
2024-01-26 16:42:31 +0100

    map page

--- frontend/src/views/client/ClientProfileView.vue
+++ frontend/src/views/client/ClientProfileView.vue
@@ -5,7 +5,7 @@
       <img class="rounded w-36 h-36" :src="userInfo.picture" alt="Extra large avatar">
       <button @click="navigateSellerHome" type="button" class="mt-5 h-fit w-fit focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Your shops</button>
     </div>
-    <user-profile-body-component :email="userInfo.email" :full_name="userInfo.name" />
+    <user-profile-body-component :email="userInfo.email" :full_name="userInfo.name + ' ' + userInfo.surname" />
     <stacked-list-component />
   </div>
 </template>
