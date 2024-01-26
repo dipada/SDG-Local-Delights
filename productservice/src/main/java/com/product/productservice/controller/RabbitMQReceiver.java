916c589b4467bea38b2c49cd2b30355117dad324
sofiama99 <sofiama@hotmail.it>
2024-01-26 11:05:07 +0100

    init product service

---
+++ productservice/src/main/java/com/product/productservice/controller/RabbitMQReceiver.java
@@ -0,0 +1,39 @@
+package com.product.productservice.controller;
+
+import com.product.productservice.repository.ProductRepository;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableRabbit
+public class RabbitMQReceiver {
+    @Autowired
+    ProductRepository productRepository;
+
+    @RabbitListener(queues = "${queue.name}")
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+    }
+
+
+
+    /*
+    @RabbitListener(queues = "${queue.name}")
+    public void addProductFromShop(ProductDetails productDetails, Long shopId) {
+        Product product = new Product();
+        product.setName(productDetails.getName());
+        product.setDescription(productDetails.getDescription());
+        product.setCategory(productDetails.getCategory());
+        product.setBrand(productDetails.getBrand());
+        product.setPrice(productDetails.getPrice());
+        product.setStock(productDetails.getStock());
+        product.setImage(productDetails.getImage());
+
+        productRepository.save(product);
+        System.out.println("Received < add product >");
+    }
+
+     */
+}
---
+++ productservice/src/main/java/com/product/productservice/dto/ProductResponse.java
@@ -0,0 +1,16 @@
+package com.product.productservice.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductResponse {
+    private Long id;
+    private Long shopId;
+    private String name;
+    private String description;
+    private String category;
+    private String brand;
+    private String image;
+    private Double price;
+    private Integer stock;
+}
