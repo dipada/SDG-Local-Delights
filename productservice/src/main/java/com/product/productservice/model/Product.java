f1537889d3ce7f83c2e25c2ed9368273cf9d374b
sofiama99 <sofiama@hotmail.it>
2024-01-26 17:00:25 +0100

    conunication between product and shop work with rabbit

--- productservice/src/main/java/com/product/productservice/model/Product.java
+++ productservice/src/main/java/com/product/productservice/model/Product.java
@@ -1,8 +1,6 @@
 package com.product.productservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Long shopId;
     private String name;
