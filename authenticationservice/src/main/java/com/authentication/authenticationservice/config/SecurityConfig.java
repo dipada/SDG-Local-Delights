0b467f3cf4060cfa6cca9636d73fff1185a70b52
dipada <daniele.dipalma68@gmail.com>
2024-01-26 18:21:44 +0100

    added logout oauth2 client

--- authenticationservice/src/main/java/com/authentication/authenticationservice/config/SecurityConfig.java
+++ authenticationservice/src/main/java/com/authentication/authenticationservice/config/SecurityConfig.java
@@ -5,24 +5,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration
 @EnableWebSecurity
+@CrossOrigin(origins = "*")
+@RequestMapping("/auth")
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-         http
-                .authorizeHttpRequests((authorize) -> {
-                    authorize.requestMatchers("/").permitAll();
-                            authorize.anyRequest().authenticated();
-                })
-            .oauth2Login(oauth2 -> oauth2
-                    .loginPage("/oauth2/authorization/google")
-                    .defaultSuccessUrl("/auth/google", true)
-                .failureUrl("/auth/failureLogin")
-        );
-       return http.build();
+        http.authorizeHttpRequests((authorize) -> {
+            authorize.requestMatchers("/", "/auth/welcome").permitAll();
+            authorize.anyRequest().authenticated();
+        }).oauth2Login(oauth2 -> oauth2.loginPage("/oauth2/authorization/google").defaultSuccessUrl("http://localhost:8080/auth/successLogin", true).failureUrl("http://localhost:8080/auth/failureLogin")).logout(logout -> logout.logoutUrl("/auth/logout").logoutSuccessUrl("/auth/logout"));
+        return http.build();
     }
 }
 
