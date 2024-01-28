d103085e359f3a45cfa0c4a553494e74dae1fcb9
dipada <daniele.dipalma68@gmail.com>
2024-01-28 12:37:51 +0100

    refactor and added openapi specification

--- productservice/src/main/java/com/product/productservice/controller/ProductController.java
+++ productservice/src/main/java/com/product/productservice/controller/ProductController.java
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,10 +23,7 @@ public class ProductController {
     private final ProductRepository productRepository;
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public ProductController(ProductRepository productRepository) {
+    public ProductController(ProductRepository productRepository, RabbitTemplate rabbitTemplate) {
         this.productRepository = productRepository;
     }
 
@@ -37,20 +35,13 @@ public class ProductController {
     @PostMapping("/add")
     public ResponseEntity<String> addProduct(@RequestBody ProductRequest productRequest) {
 
+
         Product product = new Product();
-        product.setName(productRequest.getName());
-        product.setShopId(productRequest.getShopId());
-        product.setDescription(productRequest.getDescription());
-        product.setCategory(productRequest.getCategory());
-        product.setBrand(productRequest.getBrand());
-        product.setImage(productRequest.getImage());
-        product.setPrice(productRequest.getPrice());
-        product.setStock(productRequest.getStock());
-        product.setShopId(productRequest.getShopId());
+        modifyProductParameters(productRequest, product);
 
         productRepository.save(product);
 
-        return ResponseEntity.status(200).body("Product added successfully");
+        return ResponseEntity.status(HttpStatus.OK).body("Product added successfully");
     }
 
     @Operation(summary = "Get product by Id", description = "Get product by Id ")
@@ -73,26 +64,60 @@ public class ProductController {
             productResponse.setPrice(product.get().getPrice());
             productResponse.setStock(product.get().getStock());
 
-            return ResponseEntity.status(200).body(productResponse);
+            return ResponseEntity.status(HttpStatus.OK).body(productResponse);
         }
-        return ResponseEntity.status(404).body(null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
+    @Operation(summary = "Get all products", description = "Get all products")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the products"),
+            @ApiResponse(responseCode = "404", description = "Products not found")
+    })
     @GetMapping("/all")
     public ResponseEntity<Iterable<Product>> getAllProducts() {
         Iterable<Product> products = productRepository.findAll();
         if (products.iterator().hasNext()) {
-            return ResponseEntity.status(200).body(products);
+            return ResponseEntity.status(HttpStatus.OK).body(products);
         }
-        return ResponseEntity.status(404).body(null);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 
+    @Operation(summary = "Update a product", description = "Update a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid request")
+    })
     @PutMapping("/update/{id}")
     public ResponseEntity<String> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest) {
         Product product = productRepository.findById(id).orElse(null);
         if (product == null) {
-            return ResponseEntity.status(400).body("Product not found");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
         }
+
+        modifyProductParameters(productRequest, product);
+
+        productRepository.save(product);
+
+        return ResponseEntity.status(HttpStatus.OK).body("Product updated successfully");
+    }
+
+    @Operation(summary = "Delete a product", description = "Delete a product")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Product deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Product not found")
+    })
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
+        Product product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+        productRepository.delete(product);
+        return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully");
+    }
+
+    private static void modifyProductParameters(ProductRequest productRequest, Product product) {
         product.setName(productRequest.getName());
         product.setShopId(productRequest.getShopId());
         product.setDescription(productRequest.getDescription());
@@ -101,21 +126,9 @@ public class ProductController {
         product.setImage(productRequest.getImage());
         product.setPrice(productRequest.getPrice());
         product.setStock(productRequest.getStock());
-
-        productRepository.save(product);
-
-        return ResponseEntity.status(200).body("Product updated successfully");
+        product.setShopId(productRequest.getShopId());
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
-        Product product = productRepository.findById(id).orElse(null);
-        if (product == null) {
-            return ResponseEntity.status(404).body("Product not found");
-        }
-        productRepository.delete(product);
-        return ResponseEntity.status(200).body("Product deleted successfully");
-    }
 
 
 }
