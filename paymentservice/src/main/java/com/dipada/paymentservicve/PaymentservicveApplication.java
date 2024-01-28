464ca7614dfd96d4a2c6a40cab88dce505d6787b
dipada <daniele.dipalma68@gmail.com>
2024-01-28 10:03:54 +0100

    init payment service

---
+++ paymentservice/src/main/java/com/dipada/paymentservicve/PaymentservicveApplication.java
@@ -0,0 +1,13 @@
+package com.dipada.paymentservicve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentservicveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentservicveApplication.class, args);
+    }
+
+}
---
+++ paymentservice/src/test/java/com/dipada/paymentservicve/PaymentservicveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipada.paymentservicve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentservicveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
