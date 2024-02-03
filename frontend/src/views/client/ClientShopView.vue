ef989b8657cafb032ab8bf81d5b75423fee89fff
dipada <daniele.dipalma68@gmail.com>
2024-02-03 14:44:44 +0100

    init client shop view and bind vuex state

--- frontend/src/views/client/ClientShopView.vue
+++ frontend/src/views/client/ClientShopView.vue
@@ -1,3 +1,18 @@
+<template>
+  <div>
+    <h1 class="text-black">Negozio ID: {{ shopId }}</h1>
+  </div>
+</template>
+
 <script>
+import {mapState} from 'vuex';
+
+export default {
+  name: 'ClientShopView',
+  computed: {
+    ...mapState({
+      shopId: state => state.shopId
+    })
+  }
+};
 </script>
-<template></template>
