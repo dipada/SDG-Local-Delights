6ce3bf05d2db6cb3b50d8037a12837a1a5aedeb5
dipada <daniele.dipalma68@gmail.com>
2024-02-04 14:24:42 +0100

    update profile view, added sample orders

--- frontend/src/views/client/ClientProfileView.vue
+++ frontend/src/views/client/ClientProfileView.vue
@@ -6,18 +6,52 @@
       <button @click="navigateSellerHome" type="button" class="mt-5 h-fit w-fit focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Your shops</button>
     </div>
     <user-profile-body-component :email="userInfo.email" :full_name="userInfo.name + ' ' + userInfo.surname" />
-    <stacked-list-component />
+    <OrderListComponent :orders="testOrders"/>
   </div>
 </template>
 
 <script>
-import { defineComponent, computed } from "vue";
+import { defineComponent } from "vue";
 import HeaderBase from "@/components/HeaderBase.vue";
 import UserProfileBodyComponent from "@/components/UserProfileBodyComponent.vue";
-import StackedListComponent from "@/components/StackedListComponent.vue";
+import OrderListComponent from "@/components/OrderListComponent.vue";
 
 export default defineComponent({
-  components: {StackedListComponent, UserProfileBodyComponent, HeaderBase},
+  components: {OrderListComponent, UserProfileBodyComponent, HeaderBase},
+
+  data(){
+    // 3 orders for testing purposes
+    return {
+      // TODO abbinare alla get Orders e reperire info mancanti
+      testOrders: [
+        {
+          shopname: "Shop1",
+          date: "2021-10-10",
+          amount: "100",
+          status: "completed"
+        },
+        {
+          shopname: "Shop2",
+          date: "2021-10-11",
+          amount: "200",
+          status: "cancelled"
+        },
+        {
+          shopname: "Shop3",
+          date: "2021-10-12",
+          amount: "300",
+          status: "to_be_delivered"
+        },
+        {
+          shopname: "Shop3",
+          date: "2021-10-12",
+          amount: "300",
+          status: "pending"
+        }
+      ]
+    }
+  },
+
   computed: {
     userInfo() {
       return this.$store.state.userInfo;
