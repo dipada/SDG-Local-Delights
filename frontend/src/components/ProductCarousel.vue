8e7f31c927d4494017ba427e17e1e664a8f4d004
dipada <daniele.dipalma68@gmail.com>
2024-02-05 10:09:21 +0100

    fixed cart animation and href

--- frontend/src/components/ProductCarousel.vue
+++ frontend/src/components/ProductCarousel.vue
@@ -6,20 +6,18 @@
         <div v-for="item in items" :key="item.id" class="mt-56 bg-white rounded shadow dark:bg-gray-700">
           <div class="relative z-20 p-6 group">
             <div class="relative block h-64 mb-4 -mt-56 overflow-hidden rounded -top-full ">
-              <img class="object-cover w-full h-full transition-all group-hover:scale-110"
+              <img class="pointer-events-none object-cover w-full h-full transition-all group-hover:scale-110"
                    :src="item.imageUrl" :alt="item.name">
 
               <div @click="addToCart(item.id)" class="absolute flex flex-col top-4 right-4">
-                <a href="#" class="flex items-center">
                   <div
-                      class="relative flex items-center justify-center p-3 mb-3 transition-all translate-x-20 bg-white rounded dark:bg-gray-700 dark:text-white group-hover:translate-x-0 wishlist hover:bg-blue-200 dark:hover:bg-blue-600 group">
+                      class=" active:animate-ping active:bg-green-800 relative flex items-center justify-center p-3 mb-3 transition-all translate-x-20 bg-white rounded dark:bg-gray-700 dark:text-white group-hover:translate-x-0 wishlist hover:bg-blue-200 dark:hover:bg-blue-600 group">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                          fill="green" class="bi bi-cart2" viewBox="0 0 16 16">
                       <path
                           d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"></path>
                     </svg>
                   </div>
-                </a>
               </div>
 
             </div>
