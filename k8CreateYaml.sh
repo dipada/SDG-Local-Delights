02874c53257a5062f71b2d65cb7fb1dad314e6d0
dipada <daniele.dipalma68@gmail.com>
2024-02-10 00:41:22 +0100

    update k8 script

--- k8CreateYaml.sh
+++ k8CreateYaml.sh
@@ -2,26 +2,19 @@
 
 # Set color
 YELLOW='\033[1;33m'
-# Reset color
+# reset color
 NC='\033[0m'
 
 COMPOSE_FILE="compose.yml"
 
-# Check if Kompose is installed
+# Check if kompose is installed
 if ! command -v kompose &> /dev/null
 then
-    echo -e "${YELLOW}Kompose isn't installed. Please install it and try again.${NC}"
+    echo -e "${YELLOW}Kompose isn't installed.${NC}"
     exit 1
 fi
 
 kompose convert -f "$COMPOSE_FILE"
-
-# Function to fix service names to comply with DNS-1035
-fix_service_names() {
-    sed -i 's/[-_]\([^-_]*\):/.\1:/g' $1
-    sed -i 's/\([^-_a-z0-9]\)/-/g' $1
-    sed -i 's/^[^a-z]*/service-/g' $1
-}
 
 echo "# Deployments" > all-deployments.yaml
 echo "# Services" > all-services.yaml
@@ -42,10 +35,4 @@ for f in *-persistentvolumeclaim.yaml; do
   rm -f "${f}"
 done
 
-# Apply the fix to the combined YAML files
-fix_service_names all-deployments.yaml
-fix_service_names all-services.yaml
-fix_service_names all-pvcs.yaml
-
 echo -e "${YELLOW}Creation of all-deployments.yaml, all-services.yaml, and all-pvcs.yaml completed.${NC}"
-echo -e "${YELLOW}Service names have been fixed to comply with DNS-1035.${NC}"
