9770a6a57b8d596ccb4d82e7df2c0b38bea8fe70
dipada <daniele.dipalma68@gmail.com>
2024-02-17 13:13:32 +0100

    Revert "added script that add some shops, to adapt with img and other stuff"
    This reverts commit d1ab73c1a974b70c17a748aaaa38549e33a1e277.

--- shopservice/populateShops.sh
+++ shopservice/populateShops.sh
@@ -1,7 +1,9 @@
-#! /bin/bash
+#!/bin/bash
 
+# Endpoint per l'aggiunta di un negozio
 URL="http://localhost:8082/shop/add"
 
+# Funzione per aggiungere un negozio
 add_shop() {
     curl -X POST "$URL" \
          -H "Content-Type: application/json" \
@@ -18,8 +20,12 @@ add_shop() {
          }'
 }
 
+# Chiamata della funzione add_shop per ogni negozio
+# I valori qui sono solo esempi. Sostituiscili con i dati reali dei tuoi negozi.
 add_shop "Negozio A" "Descrizione del Negozio A" "Indirizzo A, Torino" "0123456789" "emaila@negozio.it" "venditorea@negozio.it" "7.6868" "45.0703" "http://example.com/imageA.jpg"
+
 add_shop "Negozio B" "Descrizione del Negozio B" "Indirizzo B, Torino" "9876543210" "emailb@negozio.it" "venditoreb@negozio.it" "7.6968" "45.0763" "http://example.com/imageB.jpg"
+
 add_shop "Negozio C" "Descrizione del Negozio C" "Indirizzo C, Torino" "1234567890" "emailc@negozio.it" "venditorec@negozio.it" "7.7068" "45.0823" "http://example.com/imageC.jpg"
 
-echo "Shops added successfully!
+echo "I negozi sono stati aggiunti con successo."
