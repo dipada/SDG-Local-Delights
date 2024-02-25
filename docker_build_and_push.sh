78f62c656e2cffae63db006ebf2cf4d9019c14d5
dipada <daniele.dipalma68@gmail.com>
2024-02-25 17:56:34 +0100

    multi stage build, update automatized script for docker registry image push

--- docker_build_and_push.sh
+++ docker_build_and_push.sh
@@ -21,19 +21,25 @@ IMAGES=(
     "userservice:./userservice"
 )
 
-# Loop through each image
+# Build images in parallel
 for IMAGE_INFO in "${IMAGES[@]}"; do
-    # Split IMAGE_INFO into image name and directory
     IFS=":" read -r IMAGE_NAME IMAGE_DIR <<< "${IMAGE_INFO}"
     TAG="latest"
 
-    echo -e "${YELLOW} Building ${IMAGE_NAME} from ${IMAGE_DIR} ${NC}"
-    #echo "Building ${IMAGE_NAME} from ${IMAGE_DIR}"
-    docker build -t "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}" "${IMAGE_DIR}" || exit 1
+    echo -e "${YELLOW}Building ${IMAGE_NAME} from ${IMAGE_DIR}${NC}"
+    docker build --no-cache -t "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}" "${IMAGE_DIR}" &
+done
 
-    echo -e "${YELLOW} Pushing ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG} ${NC}"
-    #echo "Pushing ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}"
+wait
+echo -e "${YELLOW}All images have been build${NC}"
+
+#Push is sequential
+for IMAGE_INFO in "${IMAGES[@]}"; do
+    IFS=":" read -r IMAGE_NAME IMAGE_DIR <<< "${IMAGE_INFO}"
+    TAG="latest"
+
+    echo -e "${YELLOW}Pushing ${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}${NC}"
     docker push "${DOCKER_USERNAME}/${IMAGE_NAME}:${TAG}" || exit 1
 done
 
-echo "All images have been built and pushed successfully."
+echo -e "${YELLOW}All images have been built and pushed successfully.${NC}"
